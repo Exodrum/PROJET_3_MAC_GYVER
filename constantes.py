9027709dd1c3da63bffb0fcff88fc31f1414e22c
Exodrum <bastien.rnd@gmail.com>
2017-10-25 19:14:17 +0200

    Add constantes.py

---
+++ constantes.py
@@ -0,0 +1,10 @@
+""" Constantes of game Mac Gyver """
+
+# Settings of window
+number_sprite_height = 15
+number_sprite_width = 25
+size_sprite = 32
+size_window = number_sprite_height * size_sprite
+size_window_2 = number_sprite_width * size_sprite
+
+size_window_final = size_window + size_window_2
