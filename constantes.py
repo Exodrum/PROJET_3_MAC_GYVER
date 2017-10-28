c8109e9df5929bd1a2861ec3c70ec06b6a0884f9
Exodrum <bastien.rnd@gmail.com>
2017-10-28 12:44:22 +0200

    Ajout images items

--- constantes.py
+++ constantes.py
@@ -7,4 +7,19 @@ size_sprite = 32
 size_window = number_sprite_height * size_sprite
 size_window_2 = number_sprite_width * size_sprite
 
-size_window_final = size_window + size_window_2
+number_sprite_final = (15,25)
+
+# Game image
+#img_launcher = "img/launcher.png"
+#img_rocket = "img/rocket.png"
+img_start = "img/start.png"
+img_end = "img/guardian.png"
+img_background = "img/background.png"
+img_home = "img/home.jpg"
+img_wall = "img/wall.png"
+
+# Game head
+
+"""try_again = 0
+win = 1
+lost = 2"""
