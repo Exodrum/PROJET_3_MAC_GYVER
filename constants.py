66bfb0757ab713a676b527fac92dd7a6047e6eb9
Exodrum <bastien.rnd@gmail.com>
2017-11-23 15:22:56 +0100

    Add constants.py

--- constants.py
+++ constants.py
@@ -7,8 +7,13 @@ size_sprite = 32
 size_window = number_sprite_height * size_sprite
 size_window_2 = number_sprite_width * size_sprite
 number_sprite_final = (15,25)
-sprite_y_mac = 0
-sprite_x_mac = 4
+pos_inventory = (510,640)
+pos_win = (0, 100)
+pos_loose = (0, 100)
+
+
+# Window Image
+icone = "img/mac_gyver_right.png"
 
 # Game image
 img_launcher = "img/launcher.png"
