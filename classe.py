d01c61f2cad395f64f3ee5261a3a21ec713020f3
Bastien <32354132+Exodrum@users.noreply.github.com>
2017-12-13 17:57:24 +0100

    Update Classe.py

--- classe.py
+++ classe.py
@@ -1,15 +1,17 @@
 """ Game class for Mac Gyver Labyrinthe """
 
+import random
+
 import pygame
 from pygame.locals import *
 
 from constants import *
-import random
 
 
 class Level:
-    """ Class make a level """
-    def __init__ (self, file):
+    """Class make a level"""
+
+    def __init__(self, file):
         self.file = file # lvl.txt
         self.structure = 0
 
@@ -21,7 +23,7 @@ class Level:
             # Browse file lign
             for line in file:
                 line_level = []
-                # Browse sprites (letters) con"tains in file
+                # Browse sprites (letters) contains in file
                 for sprite in line:
                     # Ignore the "\n" of lign end
                     if sprite != '\n':
@@ -46,30 +48,29 @@ class Level:
             num_case = 0
             for sprite in lign:
                 # Calcul real position in pixels
-                x = num_case * size_sprite
-                y = num_case * size_sprite
+                pixel_x = num_case * size_sprite
+                pixel_y = num_case * size_sprite
                 if sprite == 'w':	# w = wall
-                    window.blit(wall, (x,y))
+                    window.blit(wall, (pixel_x, pixel_y))
                 if sprite == 'e':
-                	window.blit(guard, (x,y))
+                    window.blit(guard, (pixel_x, pixel_y))
                 num_case += 1
             num_lign += 1
 
-
 class MacGyver:
-    """Class make a character"""
+    """Create Mac Gyver"""
     def __init__(self, right, left, top, down, level):
         #Sprites du personnage
         self.right = pygame.image.load(right).convert_alpha()
         self.left = pygame.image.load(left).convert_alpha()
         self.top = pygame.image.load(top).convert_alpha()
         self.down = pygame.image.load(down).convert_alpha()
-        # Character pixel & cases position 
+        # Character pixel & cases position
         self.case_x = 4
         self.case_y = 0
-        self.x = 128
-        self.y = 0
-        # Direction par default
+        self.pixel_x = 128
+        self.pixel_y = 0
+        # Check good direction
         self.direction = self.right
         # Level where the character in
         self.level = level
@@ -88,7 +89,7 @@ class MacGyver:
                     # Move one case
                     self.case_x += 1
                     # Calcul real pixel position
-                    self.x = self.case_x * size_sprite
+                    self.pixel_x = self.case_x * size_sprite
             # Check image in good direction
             self.direction = self.right
 
@@ -96,72 +97,72 @@ class MacGyver:
             if self.case_x > 0:
                 if self.level.structure[self.case_y][self.case_x-1] != 'w':
                     self.case_x -= 1
-                    self.x = self.case_x * size_sprite
+                    self.pixel_x = self.case_x * size_sprite
             self.direction = self.left
 
         if direction == 'top':
             if self.case_y > 0:
                 if self.level.structure[self.case_y-1][self.case_x] != 'w':
                     self.case_y -= 1
-                    self.y = self.case_y * size_sprite
+                    self.pixel_y = self.case_y * size_sprite
             self.direction = self.top
 
         if direction == 'down':
             if self.case_y < (number_sprite_height - 1):
                 if self.level.structure[self.case_y+1][self.case_x] != 'w':
                     self.case_y += 1
-                    self.y = self.case_y * size_sprite
+                    self.pixel_y = self.case_y * size_sprite
             self.direction = self.down
 
 
-    def take_item(self, *item):
+    def take_item(self):
+        """Add one item"""
         self.item += 1
 
-    def damage(self):
-        self.health -= 1
-
     def inventory(self):
+        """Add a number of items in inventory"""
         text = "Inventory : " + str(self.item)
         return text
 
 
 class Guardian:
-    """Class make a character"""
+    """Class make guardian"""
     def __init__(self, level):
-        # Level where the character in 
         self.level = level
         self.health = 1
 
     def damage(self):
+        """Methode damage guardian"""
         self.health -= 1
 
 class Item:
     """ Class to create an item """
-    def __init__(self, name,  path, level):
+    def __init__(self, name, path, level):
         # Initial settings for the item
-        self.id = name
+        self.identification = name
         self.health = 1
         self.level = level
         self.case_x, self.case_y = self.randpos()
-        self.x = self.case_x * size_sprite
-        self.y = self.case_y * size_sprite
+        self.pixel_x = self.case_x * size_sprite
+        self.pixel_y = self.case_y * size_sprite
         self.sprite = pygame.image.load(path).convert_alpha()
 
     def randpos(self):
-        # Method to place randomly the 'Items' on the map
+        """Method to place randomly the 'Items' on the map"""
         while True:
             self.case_x = random.randrange(1, 25)
             self.case_y = random.randrange(1, 14)
             if self.level.structure[self.case_y][self.case_x] == '0':
-                self.level.structure[self.case_y][self.case_x] = self.id
+                self.level.structure[self.case_y][self.case_x] = self.identification
                 break
         return self.case_x, self.case_y
 
     def damage(self):
+        """Methode damage item on structure"""
         self.health -= 1
         self.level.structure[self.case_y][self.case_x] = '0'
 
     def display(self, window):
-        # Display the item on screen
+        """Display the item on screen"""
         if self.health > 0:
-            window.blit(self.sprite, (self.x, self.y)) 
+            window.blit(self.sprite, (self.pixel_x, self.pixel_y))
