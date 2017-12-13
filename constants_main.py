923ebf993a944e12129823fd5d2548c13ffcf936
Bastien <32354132+Exodrum@users.noreply.github.com>
2017-12-13 17:58:20 +0100

    Add constants_main.py

---
+++ constants_main.py
@@ -0,0 +1,23 @@
+"""Settings of windows"""
+number_sprite_height = 15
+number_sprite_width = 25
+size_sprite = 32
+size_window_height = number_sprite_height * size_sprite
+size_window_width = number_sprite_width * size_sprite
+pos_inventory = (10, 450)
+pos_win = (0, 0)
+pos_loose = (0, 0)
+inventory_y = 416
+inventory_x = 0
+pos_inventory = (10, 450)
+
+
+"""Window Image"""
+img_icone = "img/mac_gyver_right.png"
+window_title = "Mac Gyver Labyrinth"
+win = "img/win.png"
+loose = "img/loose.png"
+img_background = "img/background.png"
+img_home = "img/home.jpg"
+img_launcher = "img/launcher.png"
+img_rocket = "img/rocket.png"
