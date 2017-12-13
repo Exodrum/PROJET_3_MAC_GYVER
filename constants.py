5e3752bd4ec09798e74146444437f7bb09c87f90
Bastien <32354132+Exodrum@users.noreply.github.com>
2017-12-13 18:00:47 +0100

    Update contants.py

--- constants.py
+++ constants.py
@@ -1,33 +1,12 @@
-""" Constantes of game Mac Gyver """
+"""Constantes of game Mac Gyver"""
 
 # Settings of window
 number_sprite_height = 15
 number_sprite_width = 25
 size_sprite = 32
-size_window = number_sprite_height * size_sprite
-size_window_2 = number_sprite_width * size_sprite
-number_sprite_final = (15,25)
-pos_inventory = (510,640)
-pos_win = (0, 100)
-pos_loose = (0, 100)
 
-
-# Window Image
-icone = "img/mac_gyver_right.png"
 
 # Game image
-img_launcher = "img/launcher.png"
-img_rocket = "img/rocket.png"
-img_guardian = "img/guardian.png"
-img_background = "img/background.png"
-img_home = "img/home.jpg"
+img_guard = "img/guardian.png"
 img_wall = "img/wall.png"
-img_win = "img/win.png"
-img_loose = "img/loose.png"
-
-
-#Labyrinth structure symbols
-wall = 'w'
-floor = '0'
-item = 'i'
-
+img_icone = "img/mac_gyver_right.png"
